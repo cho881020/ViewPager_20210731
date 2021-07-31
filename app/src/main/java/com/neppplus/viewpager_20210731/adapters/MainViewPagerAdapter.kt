c7356dcf7da3c94461c1b1e905d7afe0399e9fe8
조경진 <cho881020@gmail.com>
2021-07-31 11:04:09 +0900

    페이지별 제목도 추가

--- app/src/main/java/com/neppplus/viewpager_20210731/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_20210731/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,20 @@ import com.neppplus.viewpager_20210731.fragments.NameFragment
 
 class MainViewPagerAdapter( fm : FragmentManager ) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+
+        if (position == 0) {
+            return "이름"
+        }
+        else if (position == 1) {
+            return "정보"
+        }
+        else {
+            return "인사말"
+        }
+
+    }
+
     override fun getCount(): Int {
         return 3
     }
